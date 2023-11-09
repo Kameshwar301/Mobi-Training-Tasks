88a7f8a2b0a2491c3dbef5924ad7275aa5c10949
Kameshwar301 <sarankamesh301@gmail.com>
2023-11-09 12:15:30 +0530

    overriding Animal Family Cat Bird

---
+++ Test1.java
@@ -0,0 +1,13 @@
+package training.day1;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Cat c = new Cat();
+		Bird b = new Bird();
+		c.sound();
+		b.sound();
+	}
+
+}
