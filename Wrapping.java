d9c49aadc4a5bb784e5f855835905bd7b4610662
Kameshwar301 <sarankamesh301@gmail.com>
2023-11-09 15:11:23 +0530

    Primitive to Object

---
+++ Wrapping.java
@@ -0,0 +1,13 @@
+package training.day1;
+
+public class Wrapping {
+
+	public static void main(String[] args) {
+		int i = 10;
+		
+		Integer a = i;
+		Integer b = Integer.valueOf(i);
+		System.out.println(i+ " "+a+" "+b);
+	}
+
+}
